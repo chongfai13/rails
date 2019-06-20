8e3c8b44e9dcab5672053755b543f563c2166e64
Chong Hwi <chongfai13@gmail.com>
2019-06-20 17:54:19 +0800

    complete2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+    http_basic_authenticate_with name:"chong", password: "hwi", except: [:index, :show]
+
     def index
         @articles = Article.all
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles
+  root 'articles#index'
 
-  root 'welcome#index'
+  resources :articles do
+  resources :comments
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
